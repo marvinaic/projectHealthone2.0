bac8e10f5b4db140ce0b1064c3a2b0ca34d93fd4
marvinaic <302817759@student.rocmondrian.nl>
2020-12-11 14:50:39 +0100

    secondCommit2.0

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -1,7 +1,38 @@
 <?php
 
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\Routing\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class HomepageController
 {
+    /**
+     * @\Symfony\Component\Routing\Annotation\Route("/")
+     */
+    public function homepage()
+    {
+        return new Response("home");
+    }
+    /**
+     * @\Symfony\Component\Routing\Annotation\Route("/medication")
+     */
+    public function displayMedication()
+    {
+        return new Response("medicine");
+    }
+   /* /**
+      * @Route("/news/{slug}")
+      */
+    /*
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show the article: "%s"',
+            $slug
+        ));
+    }
+   */
 
 }
