c860a1a90d231b03223f9d550fda9b44ab7b3d5e
marvinaic <302817759@student.rocmondrian.nl>
2020-12-13 21:14:30 +0100

    opdracht7

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -3,21 +3,32 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 //use Symfony\Component\Routing\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+//use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class HomepageController
+class HomepageController extends AbstractController
 {
     /**
      * @\Symfony\Component\Routing\Annotation\Route("/")
      */
     public function homepage()
     {
-        return new Response("home");
+        //return $this->render('home.html.twig', [ ]);
+        $medication = [
+            'pil',
+            'zalf',
+            'prik',
+        ];
+        return $this->render('medication.html.twig', [
+            'medication' => $medication,
+        ]);
     }
+    /*
     /**
      * @\Symfony\Component\Routing\Annotation\Route("/medication")
      */
+    /*
     public function displayMedication()
     {
         return new Response("medicine");
